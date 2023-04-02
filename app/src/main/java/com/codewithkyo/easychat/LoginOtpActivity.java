d60e6bc804b5869138f6618c278ad4b1ed5b6d4a
YamamotoDesu <mynameisyamamotodesu@gmail.com>
2023-04-03 08:19:01 +0900

    4 Login with Phone number | Chat application | Android Studio

--- app/src/main/java/com/codewithkyo/easychat/LoginOtpActivity.java
+++ app/src/main/java/com/codewithkyo/easychat/LoginOtpActivity.java
@@ -3,12 +3,19 @@ package com.codewithkyo.easychat;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class LoginOtpActivity extends AppCompatActivity {
+
+    String phoneNumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_username);
+
+        phoneNumber = getIntent().getExtras().getString("phone");
+        Toast.makeText(getApplicationContext(), phoneNumber, Toast.LENGTH_LONG).show();
+
     }
 }
